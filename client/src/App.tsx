ab9edef7e60b358bb18bb90ef0de30ede2f46c08
sourish19 <sourish19z@gmail.com>
2025-11-11 16:29:40 +0000

    feat: Initialized routing & layouts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
-import { Button } from "@/components/ui/button"
+import { Routes, Route } from 'react-router-dom';
+import Signin from './components/auth/Signin';
+import Signup from './components/auth/Signup';
+import Dashboard from './components/Dashboard';
+import Send from './components/Send';
+import PublicLayout from './layouts/PublicLayout';
+import PrivateLayout from './layouts/PrivateLayout';
 
-function App() {
+const App = () => {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button type="button" className="bg-black text-white">Click me</Button>
-    </div>
-  )
-}
+    <Routes>
+      <Route element={<PublicLayout />}>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+      </Route>
+      <Route element={<PrivateLayout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/send" element={<Send />} />
+      </Route>
+    </Routes>
+  );
+};
 
-export default App
+export default App;
